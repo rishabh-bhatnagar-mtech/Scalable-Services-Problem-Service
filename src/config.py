3fe90858012929ab19ee8b69cf6b2289bce8376a
Rishabh Bhatnagar <2023tm93760@wilp.bits-pilani.ac.in>
2025-04-29 14:07:39 +0000

    Add initial config with db and makefile for export
    Signed-off-by: Rishabh Bhatnagar <2023tm93760@wilp.bits-pilani.ac.in>

---
+++ src/config.py
@@ -0,0 +1,18 @@
+import os
+
+from pydantic import BaseSettings
+
+
+class Settings(BaseSettings):
+    DB_HOST: str = os.getenv("DB_HOST", "localhost")
+    DB_PORT: str = os.getenv("DB_PORT", "5432")
+    DB_USER: str = os.getenv("DB_USER", "postgres")
+    DB_PASSWORD: str = os.getenv("DB_PASSWORD", "postgres")
+    DB_NAME: str = os.getenv("DB_NAME", "problems")
+
+    @property
+    def DATABASE_URL(self):
+        return f"postgresql+psycopg2://{self.DB_USER}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+
+
+settings = Settings()
