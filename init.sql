e336913ef262a388d1731c2532dcc7cf867d1d19
rishabh-bhatnagar-mtech <2023tm93760@wilp.bits-pilani.ac.in>
2025-05-03 10:19:55 +0530

    Remove slug from the DB Schema
    Signed-off-by: rishabh-bhatnagar-mtech <2023tm93760@wilp.bits-pilani.ac.in>

--- init.sql
+++ init.sql
@@ -1,12 +1,11 @@
 CREATE TABLE IF NOT EXISTS problems
 (
     id           SERIAL PRIMARY KEY,
-    title        VARCHAR(255)        NOT NULL,
-    slug         VARCHAR(255) UNIQUE NOT NULL,
+    title        VARCHAR(255) NOT NULL,
     difficulty   VARCHAR(20) CHECK (difficulty IN ('easy', 'medium', 'hard')),
-    description  TEXT                NOT NULL,
-    starter_code JSONB               NOT NULL,
-    test_cases   JSONB               NOT NULL,
+    description  TEXT         NOT NULL,
+    starter_code JSONB        NOT NULL,
+    test_cases   JSONB        NOT NULL,
     constraints  TEXT[]      DEFAULT ARRAY []::TEXT[],
     created_at   TIMESTAMPTZ DEFAULT NOW(),
     updated_at   TIMESTAMPTZ DEFAULT NOW()
