682d4a654d6356f3e5920ef2e9126feae06467d0
rishabh-bhatnagar-mtech <2023tm93760@wilp.bits-pilani.ac.in>
2025-05-03 14:04:37 +0530

    Externally supply env file path
    Signed-off-by: rishabh-bhatnagar-mtech <2023tm93760@wilp.bits-pilani.ac.in>

--- src/core/config.py
+++ src/core/config.py
@@ -1,6 +1,7 @@
 import pathlib
 
-from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import ConfigDict
+from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
@@ -13,10 +14,13 @@ class Settings(BaseSettings):
     def get_database_url(self) -> str:
         return f"postgresql+psycopg://{self.DB_USER}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
 
-    model_config = SettingsConfigDict(
-        env_file=f"{pathlib.Path(__file__).resolve().parent.parent.parent}/deployments/.env",
-        env_file_encoding="utf-8",
-    )
+    print(f"{pathlib.Path(__file__).resolve().parent.parent.parent}/deployments/.env", )
+
+    model_config = ConfigDict(extra='allow')
 
 
-settings = Settings()
+def get_settings(env_file_path: str = None) -> Settings:
+    if env_file_path is None:
+        env_file_path = f"{pathlib.Path(__file__).resolve().parent.parent.parent}/deployments/.env"
+    settings = Settings(_env_file=env_file_path)
+    return settings
--- src/dependencies/database.py
+++ src/dependencies/database.py
@@ -1,14 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from core.config import settings
-
-engine = create_engine(settings.get_database_url())
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+from core.config import get_settings, Settings
 
 
-def get_db():
-    db = SessionLocal()
+def get_db(settings: Settings):
+    engine = create_engine(settings.get_database_url())
+    session_local = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+    db = session_local()
     try:
         yield db
     finally:
